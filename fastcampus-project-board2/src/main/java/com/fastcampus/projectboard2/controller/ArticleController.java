37b752f3198a6379fbd6c1994442050063149af9
oycoyj2 <oycoyj2@gmail.com>
2024-02-28 13:20:23 +0900

    #16 - 자잘자잘
    자잘자잘

--- fastcampus-project-board2/src/main/java/com/fastcampus/projectboard2/controller/ArticleController.java
+++ fastcampus-project-board2/src/main/java/com/fastcampus/projectboard2/controller/ArticleController.java
@@ -1,9 +1,29 @@
 package com.fastcampus.projectboard2.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+    @GetMapping
+    public String articles(ModelMap map) {
+        map.addAttribute("articles", List.of());
+
+        return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String articles(@PathVariable Long articleId, ModelMap map) {
+        map.addAttribute("article", null);
+        map.addAttribute("articleComments", List.of());
+
+        return "articles/detail";
+    }
 }

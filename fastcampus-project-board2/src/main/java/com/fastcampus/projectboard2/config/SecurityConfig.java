ab50b68f882b2351a268dad78c593fd5c1ff66ac
oycoyj2 <oycoyj2@gmail.com>
2024-02-28 14:12:14 +0900

    #16 - SecurityConfig 추가
    SpringSecurity를 설정해주기 위해 컨피크 파일을 추가했다. 강의와는 또 다르기 때문에 구글링을 해서 설정을 추가했다(람다식 이용)

---
+++ fastcampus-project-board2/src/main/java/com/fastcampus/projectboard2/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.fastcampus.projectboard2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+        http
+                .authorizeHttpRequests(
+                        authorize -> authorize
+                                .requestMatchers("/**").permitAll()
+                                .requestMatchers("/articles").permitAll()
+                                .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}

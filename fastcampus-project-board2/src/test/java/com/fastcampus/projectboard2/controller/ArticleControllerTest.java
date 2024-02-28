8f1dbf16d6854867859faf6eb8a66d351fea91d3
oycoyj2 <oycoyj2@gmail.com>
2024-02-28 11:01:13 +0900

    #16 - 게시판 뷰 테스트 내용 업데이트
    기존 테스트를 실패한 채 올리면 'gradle build' 가 실패하여 배포 자동화에 악영향을 미치는 경우가 있으므로, 실패 테스트는 제외하기로 했다.
    이에 기능 구현 중인 건 '@Disabled' 처리했다.(안함)
    
    그 밖에 생각난 기능을 업데이트

--- fastcampus-project-board2/src/test/java/com/fastcampus/projectboard2/controller/ArticleControllerTest.java
+++ fastcampus-project-board2/src/test/java/com/fastcampus/projectboard2/controller/ArticleControllerTest.java
@@ -30,6 +30,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
 
     }
@@ -43,7 +44,9 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(model().attributeExists("article"));
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
 
     }
 
@@ -56,7 +59,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(model().attributeExists("article"));
+                .andExpect(model().attributeExists("articles/search"));
 
     }
 
@@ -69,7 +72,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(model().attributeExists("article"));
+                .andExpect(model().attributeExists("article/search-hashtag"));
 
     }
 }
